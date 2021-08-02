cfb6c7c6e4ff9878a2f84e1e19158528484b23ff
Reiramisuki <62616500+Reiramisuki@users.noreply.github.com>
2021-08-02 15:22:01 -0300

    Add files via upload

---
+++ TPs.Silva.DDAW/src/main/java/ar/org/centro8/curso/java/web/services/JaxRSConfig.java
@@ -0,0 +1,9 @@
+package ar.org.centro8.curso.java.web.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/webresourses") 
+public class JaxRSConfig extends Application{
+    
+}
